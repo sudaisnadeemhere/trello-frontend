3dc591e2bd812c6f4a347ddc0a87a639a2209342
sudaisnadeemhere <sudais731@gmail.com>
2026-06-23 04:37:07 -0700

    done

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://trello-backend-five.vercel.app',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
